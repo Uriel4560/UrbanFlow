4d795d25a4ae08f0e74d688cc881303deea4cd29
Uriel <U23227010@utp.edu.pe>
2026-06-29 23:26:05 -0500

    Agregar perfil de prueba para H2 in-memory

--- src/test/java/com/ventas/ropa/sistema/SistemaApplicationTests.java
+++ src/test/java/com/ventas/ropa/sistema/SistemaApplicationTests.java
@@ -2,8 +2,10 @@ package com.ventas.ropa.sistema;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SistemaApplicationTests {
 
 	@Test
